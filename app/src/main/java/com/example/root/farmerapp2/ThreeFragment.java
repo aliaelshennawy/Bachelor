95d591b77eb828f3751509fed8a124271e123cc0
aliaelshennawy <alia.elshennawy@gmail.com>
2016-03-16 14:04:42 +0200

    audio capture/not connected

---
+++ app/src/main/java/com/example/root/farmerapp2/ThreeFragment.java
@@ -0,0 +1,147 @@
+package com.example.root.farmerapp2;
+
+/**
+ * Created by root on 10/03/16.
+ */
+
+
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.example.root.farmerapp2.R;
+
+import java.io.IOException;
+
+
+public class ThreeFragment extends Fragment{
+    Button play,stop,record;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile = null;
+    private RelativeLayout Rlayout;
+    private FragmentActivity faActivity;
+    public ThreeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        faActivity  = (FragmentActivity)    super.getActivity();
+        Rlayout    = (RelativeLayout)    inflater.inflate(R.layout.fragment_three, container, false);
+
+        play=(Button)Rlayout.findViewById(R.id.button3);
+        stop=(Button)Rlayout.findViewById(R.id.button2);
+        record=(Button)Rlayout.findViewById(R.id.button);
+
+        stop.setEnabled(false);
+        play.setEnabled(false);
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";;
+
+        myAudioRecorder=new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    myAudioRecorder.prepare();
+                    myAudioRecorder.start();
+                }
+
+                catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                record.setEnabled(false);
+                stop.setEnabled(true);
+
+                Toast.makeText(getActivity().getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioRecorder.stop();
+                myAudioRecorder.release();
+                myAudioRecorder  = null;
+
+                stop.setEnabled(false);
+                play.setEnabled(true);
+
+                Toast.makeText(getActivity().getApplicationContext(), "Audio recorded successfully",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) throws IllegalArgumentException,SecurityException,IllegalStateException {
+                MediaPlayer m = new MediaPlayer();
+
+                try {
+                    m.setDataSource(outputFile);
+                }
+
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    m.prepare();
+                }
+
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                m.start();
+                Toast.makeText(getActivity().getApplicationContext(), "Playing audio", Toast.LENGTH_LONG).show();
+            }
+        });
+        return Rlayout;
+    }
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/root/farmerapp2/TwoFragment.java
@@ -0,0 +1,55 @@
+package com.example.root.farmerapp2;
+
+/**
+ * Created by root on 10/03/16.
+ */
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+
+import android.os.Bundle;
+import android.os.Environment;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import android.widget.ImageView;
+import android.widget.Toast;
+import java.io.IOException;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.root.farmerapp2.R;
+
+
+public class TwoFragment extends Fragment implements View.OnClickListener{
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
